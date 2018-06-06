b84b9ab8cda2d8ae88f8db5dff20fced1d5ec6a9
Nir Koren <nirk@liveperson.com>
2018-06-06 11:10:04 +0300

    init commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Welcome to SI-SE Zurich 2018 demo repo
+## This repo contains basic Maven project with Hello-World WAR file 
+<BR> Ready for Tomcat deployment 
+<BR> now also support jetty
+
---
+++ build-war/src/main/java/org/nirkoren/maven/demo/HelloHandler.java
@@ -0,0 +1,10 @@
+package org.nirkoren.maven.demo;
+
+public class HelloHandler {
+
+	public String sayHello() {
+		String hello = "Hello SI-SE members, What do you think about this conference?";
+		return hello;
+	}
+
+}
