eb983b6a43bc0e3b959a9dee6c28daebe230a161
Nir Koren <hanirnir@gmail.com>
2018-06-20 10:01:12 +0200

    Change a word

--- build-war/src/main/java/org/nirkoren/maven/demo/HelloHandler.java
+++ build-war/src/main/java/org/nirkoren/maven/demo/HelloHandler.java
@@ -3,7 +3,7 @@ package org.nirkoren.maven.demo;
 public class HelloHandler {
 
 	public String sayHello() {
-		String hello = "Hello SI-SE members, What do you think about this conference?";
+		String hello = "Dear SI-SE members, What do you think about this conference?";
 		return hello;
 	}
 
